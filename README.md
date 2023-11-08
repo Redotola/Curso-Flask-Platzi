cbb5d5cb95c75f099cee7a408d086c51e0e1e618
= <=>
2023-11-08 16:24:24 -0600

    Flask

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Curso-Flask-Platzi
+In this course I learn about the Micro Framework "Flask" that is really useful web tool
+
+# Create environment
+virtualenv "name"
+
+# Install the packages
+pip install -r requirements
+
+# Create the terminal variable to run application
+( Linux )
+export FLASK_APP=app.py
+
+( Windows )
+set FLASK_APP=app.py
+
+# Verify the variable was created
+( Linux )
+echo $FLASK_APP
+
+( Windows )
+echo FLASK_APP
+
+# Run the server 
+flask run
--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
-app = Flask(__name__)
+app = Flask(__name__) # the app name is the name of the current file
 
+@app.route('/') # route of the function is working
+def index():
+    
+    user_ip = request.remote_addr # obtain the ip of the user
+    response = make_response(redirect('/hello')) # the index redirect to 'hello' route
+    response.set_cookie("user_ip", user_ip) # set the cookie in the browser with a name
+    return response
+    
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip') # obtain the cookie from the browser
+    return f"Hello World Platzi, tu IP es {user_ip}"
+
+if __name__ == "__main__": # if the file is explicit execute the server run
+    app.run(port = 5500, host = "0.0.0.0", debug = True)
